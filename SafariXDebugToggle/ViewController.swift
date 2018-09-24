631c1dd1abe870145c2ac19bfd5fae909e7469f5
Benjamin Issleib <bi@converia.de>
2018-09-24 11:09:04 +0200

    Initial Commit

---
+++ SafariXDebugToggle/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SafariXDebugToggle
+//
+//  Created by Benjamin Issleib on 24.09.18.
+//  Copyright © 2018 Benjamin Issleib. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
